6b8243d38576f05be509ad5a18fdf5f25fa7ebe3
synsi <mail@himitsu.dev>
2020-05-20 21:24:00 +0200

    Added Semaphore, MailBox, IdleHook,

--- synos8/synos.cpp
+++ synos8/synos.cpp
@@ -12,12 +12,13 @@ using namespace syn;
 Routine Kernel::_routinelist[SYN_OS_ROUTINE_COUNT];
 uint8_t Kernel::_current_ticks_left;
 uint8_t Kernel::_readycount;
+uint8_t Kernel::_isr_reschedule_request;
 
 static Routine* _current_routine;
 static uint8_t* _mainstack;
 
-#if (SYN_OS_TIMER_COUNT > 0)
-SoftTimer SoftTimer::_timerlist[SYN_OS_TIMER_COUNT];
+#if (SYN_OS_TICK_HOOK_COUNT > 0)
+SysTickHook SysTickHook::_timerlist[SYN_OS_TICK_HOOK_COUNT];
 #endif
 
 void Routine::_init(void* functor, void* arg, uint8_t* stack)
@@ -68,37 +69,101 @@ void Routine::_init(void* functor, void* arg, uint8_t* stack)
   _stackptr = stack;
 }
 
-void Routine::pause()
+void Routine::yield()
 {
-  Atomic a; // disable interrupts
-  _current_routine->_id_runnable &= 0xFE; // clear runnable bit
-  --Kernel::_readycount; // decrease ready count
-  Kernel::_contextSwitch(); // switch out the routine cooperatively
+  Atomic a;
+  Kernel::_contextYield(false);
 }
 
-#if (SYN_OS_TIMER_COUNT > 0)
-void SoftTimer::_init(SoftTimer::timer_functor_t functor, uint8_t reload)
+bool Routine::is_runnable() const
+{
+  return _id_runnable & 0x01;
+}
+
+void Routine::block()
+{
+  _id_runnable &= 0x7E;
+  --Kernel::_readycount; // decrease ready count
+}
+
+void Routine::unblock()
+{
+  _id_runnable |= 0x01;
+  ++Kernel::_readycount; // increase ready count
+}
+
+
+#if (SYN_OS_TICK_HOOK_COUNT > 0)
+void SysTickHook::_init(SysTickHook::timer_functor_t functor, uint8_t reload)
 {
   _next_exec_time = reload;
   _reload = reload;
   _functor = functor;
 }
 
-void SoftTimer::_checkAndExec()
+void SysTickHook::_checkAndExec()
 {
   uint8_t millis = System::millis8();
-  SoftTimer *ptim = _timerlist;
+  SysTickHook *ptim = _timerlist;
   do
   {
     if(millis == ptim->_next_exec_time)
     {
       ptim->_next_exec_time = millis + ptim->_reload;
+      assert(ptim->_functor != 0); // to many hooks cofigured in synhal_cfg
       ptim->_functor();
     }
     ++ptim;
-  } while(ptim != &_timerlist[SYN_OS_TIMER_COUNT]);
+  } while(ptim != &_timerlist[SYN_OS_TICK_HOOK_COUNT]);
 }
-#endif // (SYN_OS_TIMER_COUNT > 0)
+#endif // (SYN_OS_TICK_HOOK_COUNT > 0)
+
+Semaphore::Semaphore(uint8_t start)
+{
+  _count = start;
+}
+
+void Semaphore::give()
+{
+  Atomic a;
+  give_isr();
+}
+
+void Semaphore::give_isr()
+{
+  // already critical section, dont need Atomic
+  ++_count;
+  assert(_count != 0); // watch for overflow
+  // unconditionally unblockrecurse, Kernel will know best what to do
+  Kernel::_unblockWaitlist(_waitlist);
+}
+
+void Semaphore::get()
+{
+  Atomic a;
+  while(_count == 0)
+  {
+    Kernel::_enterWaitlist(_waitlist);
+  }
+  --_count;
+}
+
+bool Semaphore::get_isr()
+{
+  bool ret = false;
+  if(_count > 0)
+  {
+    --_count;
+    ret = true;
+  }
+  return ret;
+}
+
+bool Semaphore::try_get()
+{
+  Atomic a;
+  return get_isr();
+}
 
 void Kernel::init()
 {
@@ -127,6 +192,11 @@ void Kernel::spin()
   archFirstThreadRestore(_routinelist);
 }
 
+bool Kernel::is_idle()
+{
+  return _current_routine == (Routine*)&_mainstack;
+}
+
 // transform stack point to point at the top
 // add known bytes if stack check is enabled
 uint8_t* Kernel::_base_stack_setup(uint8_t* stack, uint16_t size)
@@ -145,11 +215,74 @@ uint8_t* Kernel::_base_stack_setup(uint8_t* stack, uint16_t size)
 
 void Kernel::_idle()
 {
-  // TODO dont just idle, run Timers
-  uint16_t x;
   while(1)
-    ++x;
-  //wfi();
+    syn_os_idle_hook();
+}
+
+void Kernel::exit_isr()
+{
+  if(_isr_reschedule_request & 0x80)
+  {
+    // request 0x80 is set when an interrupt enables a routine
+    // but since we don't practice eager preemption, this only
+    // happens when we are currently idle. and only happens once
+    // _current_routine already points to the new routine, so just
+    // reschedule (delayed) the whole purpose of this is to not interrupt an isr
+#ifndef SYN_OS_ROUTINE_RR_SLICE_MS
+    _current_ticks_left = _current_routine->_rr_reload;
+#else
+    _current_ticks_left = SYN_OS_ROUTINE_RR_SLICE_MS / SYN_SYSTICK_FREQ;
+#endif
+    archContextSwitch((Routine*)&_mainstack, _current_routine);
+  }
+  _isr_reschedule_request = 0;
+}
+
+void Kernel::_enterWaitlist(Routine*& listhead)
+{
+  if(listhead == 0)
+  {
+    listhead = _current_routine;
+    _current_routine->_next = 0;
+  }
+  else
+  {
+    Routine *tmp = listhead;
+    while(tmp->_next != 0)
+    {
+      tmp = tmp->_next;
+    }
+    tmp->_next = _current_routine;
+    _current_routine->_next = 0;
+  }
+  _contextYield(true);
+}
+
+void Kernel::_unblockWaitlist(Routine*& listhead)
+{
+  if(listhead != 0)
+  {
+    Routine *prt = listhead;
+    prt->unblock();
+    listhead = prt->_next;
+    _contextUnblocked(prt);
+  }
+}
+
+void Kernel::_unblockWaitlistAll(Routine*& listhead)
+{
+  if(listhead != 0)
+  {
+    Routine *prt = listhead;
+    do
+    {
+      prt->unblock();
+      prt = prt->_next;
+    } while (prt != 0);
+    prt = listhead;
+    listhead = 0;
+    _contextUnblocked(prt);
+  }
 }
 
 void Kernel::_contextSwitch()
@@ -189,46 +322,104 @@ void Kernel::_contextSwitch()
   // else idle task already running and no runnables, do nothing
 }
 
-void Kernel::_tickySwitch()
+// method to use if a routine gives up control by itself
+// either through yielding or by some blocking action
+// paramter defines wether or not the routine can continue
+void Kernel::_contextYield(bool current_is_block)
 {
-  --_current_ticks_left;
-  if(_current_ticks_left == 0)
+  if(current_is_block)
   {
-    if(_readycount == 1 && _current_routine->_id_runnable & 0x01)
+    _current_routine->block();
+    _contextSwitch();
+  }
+  else if (_readycount != 1)
+  {
+    // dont switch if this is the only routine runnable anyway
+    _contextSwitch();
+  }
+}
+
+// optimized context switch when we know some routine just got unblocked
+void Kernel::_contextUnblocked(Routine* unblocked)
+{
+  // no aggressive preemption, we only switch the context when idle
+  if(is_idle())
+  {
+    _current_routine = unblocked;
+    if(_isr_reschedule_request == 0)
     {
-      // no switch needed, just refresh this routines tick count
+      // not an isr context, so switch right away
 #ifndef SYN_OS_ROUTINE_RR_SLICE_MS
       _current_ticks_left = _current_routine->_rr_reload;
 #else
       _current_ticks_left = SYN_OS_ROUTINE_RR_SLICE_MS / SYN_SYSTICK_FREQ;
 #endif
+      archContextSwitch((Routine*)&_mainstack, _current_routine);
     }
     else
     {
-      _contextSwitch();
+      // set the isr contextswitch pending bit
+      // dont care to overwrite the old value, it is forbidden to nest
+      _isr_reschedule_request = 0x80;
     }
   }
 }
 
-void Kernel::_funkySwitch()
+void Kernel::_tickySwitch()
 {
-  _contextSwitch();
+  if(_isr_reschedule_request & 0x80)
+  {
+    _isr_reschedule_request = 0;
+    // request 0x80 is set when an interrupt enables a routine
+    // but since we don't practice eager preemption, this only
+    // happens when we are currently idle. and only happens once
+    // _current_routine already points to the new routine, so just
+    // reschedule (delayed) the whole purpose of this is to not interrupt an isr
+#ifndef SYN_OS_ROUTINE_RR_SLICE_MS
+    _current_ticks_left = _current_routine->_rr_reload;
+#else
+    _current_ticks_left = SYN_OS_ROUTINE_RR_SLICE_MS / SYN_SYSTICK_FREQ;
+#endif
+    archContextSwitch((Routine*)&_mainstack, _current_routine);
+  }
+  else
+  {
+    _isr_reschedule_request = 0;
+    --_current_ticks_left;
+    if(_current_ticks_left == 0)
+    {
+      if(_readycount == 1 && _current_routine->is_runnable())
+      {
+        // no switch needed, just refresh this routines tick count
+#ifndef SYN_OS_ROUTINE_RR_SLICE_MS
+        _current_ticks_left = _current_routine->_rr_reload;
+#else
+        _current_ticks_left = SYN_OS_ROUTINE_RR_SLICE_MS / SYN_SYSTICK_FREQ;
+#endif
+      }
+      else
+      {
+        _contextSwitch();
+      }
+    }
+  }
 }
 
-INTERRUPT_HANDLER_TRAP(TRAP) {
-  Kernel::_funkySwitch();
-}
+//INTERRUPT_HANDLER_TRAP(TRAP) {
+//  Kernel::_funkySwitch();
+//}
 
 INTERRUPT_HANDLER(TIMER4_OV, 23) {
+  Kernel::enter_isr();
   System::_systick_isr();
-#if (SYN_OS_TIMER_COUNT > 0)
+#if (SYN_OS_TICK_HOOK_COUNT > 0)
 #ifdef SYN_OS_RUN_TIMER_ON_MAINSTACK
-  if(_current_routine != (Routine*)&_mainstack)
-    synRunOnMainStack((void*)&SoftTimer::_checkAndExec, _mainstack);
+  if(Kernel::is_idle())
+    SysTickHook::_checkAndExec();
   else
-    SoftTimer::_checkAndExec();
+    synRunOnMainStack((void*)&SysTickHook::_checkAndExec, _mainstack);
 #else
-  SoftTimer::_checkAndExec();
+  SysTickHook::_checkAndExec();
 #endif
 #endif
   Kernel::_tickySwitch();
--- synos8/synos.h
+++ synos8/synos.h
@@ -29,24 +29,26 @@ namespace syn
   public:
     template<uint8_t Index, typename Functor_t, typename Argument_t>
     static void init(Functor_t functor, Argument_t arg, uint8_t* stack, uint16_t stacksize, uint8_t rr_millis);
-  
-    static void yield()
-    {
-      trap();
-    }
 
-    // mark the currently running routine as not runnable
-    static void pause();
+    // leave execution context if other routinbe is runnable
+    static void yield();
+
+    bool is_runnable() const;
   private:
     void _init(void* functor, void* arg, uint8_t* stack);
+
+    void block();
+    void unblock();
+
     uint8_t* _stackptr;
     uint8_t _id_runnable;
 #ifndef SYN_OS_ROUTINE_RR_SLICE_MS
     uint8_t _rr_reload;
 #endif
+    Routine* _next;
   };
 
-  class SoftTimer
+  class SysTickHook
   {
   public:
     typedef void(*timer_functor_t)(void);
@@ -59,37 +61,243 @@ namespace syn
   private:
     void _init(timer_functor_t functor, uint8_t reload);
 
-    static SoftTimer _timerlist[SYN_OS_TIMER_COUNT];
+    static SysTickHook _timerlist[SYN_OS_TICK_HOOK_COUNT];
 
     uint8_t _next_exec_time;
     uint8_t _reload;
     timer_functor_t _functor;
   };
 
+  class Semaphore
+  {
+  public:
+    Semaphore(uint8_t start = 0);
+
+    void give();
+    void give_isr();
+
+    void get();
+    bool get_isr();
+    bool try_get();
+
+    uint8_t count() const
+    {
+      return _count;
+    }
+  private:
+    uint8_t _count;
+    Routine* _waitlist;
+  };
+
   class Kernel
   {
     friend class Routine;
+    friend class Semaphore;
   public:
     static void init();
     static void spin();
     static Routine& currentRoutine();
 
+    static bool is_idle();
+
+    static void enter_isr()
+    {
+      assert(_isr_reschedule_request == 0);
+      ++_isr_reschedule_request;
+    }
+    static void exit_isr();
+
     static void _tickySwitch();
-    static void _funkySwitch();
   private:
+    // general purpose context switch that always scedules next runnable, or idle
+    // however, next runnable could also be the same routine.
+    // so its not always smart to call, but always safe
     static void _contextSwitch();
+    // optimized function to call if the current routine exits and might be blocked
+    static void _contextYield(bool routine_shall_block);
+    // optimized function to call when we already know something unblocked
+    static void _contextUnblocked(Routine *unblocked);
+
     static uint8_t* _base_stack_setup(uint8_t* stack, uint16_t size);
     static void _idle();
+
+    // put current Routine into the list, reshedule. Not allowed by ISR
+    static void _enterWaitlist(Routine*& listhead);
+    // unblock the head and reschedule if idle
+    static void _unblockWaitlist(Routine*& listhead);
+    // remove unblock the entire list and schedule the head if idle
+    static void _unblockWaitlistAll(Routine*& listhead);
+
 
     static Routine _routinelist[SYN_OS_ROUTINE_COUNT];
     static uint8_t _current_ticks_left;
     static uint8_t _readycount;
+    static uint8_t _isr_reschedule_request;
+  };
+
+  template<typename Mail_t, uint8_t Size>
+  class MailBox
+  {
+  public:
+    MailBox()
+    {
+      _pread = _pwrite = _mails;
+    }
+
+    bool push_isr(const Mail_t& mail)
+    {
+      bool ret = false;
+      if(_signal.count() != Size)
+      {
+        *_pwrite++ = mail;
+        if(_pwrite == &_mails[Size])
+          _pwrite = _mails;
+        _signal.give_isr();
+        ret = true;
+      }
+      return ret;
+    }
+
+    bool try_push(const Mail_t& mail)
+    {
+      Atomic a;
+      return push_isr(mail);
+    }
+
+    void push(const Mail_t& mail)
+    {
+      Atomic a;
+      while(!push_isr(mail))
+        Routine::yield();
+    }
+
+    bool pop_isr(Mail_t& mail)
+    {
+      bool ret = false;
+      if(_signal.try_get())
+      {
+        mail = *_pread++;
+        if(_pread == &_mails[Size])
+          _pread = _mails;
+        ret = true;
+      }
+      return ret;
+    }
+
+    bool try_pop(Mail_t& mail)
+    {
+      Atomic a;
+      return pop_isr(mail);
+    }
+
+    void pop(Mail_t& mail)
+    {
+      Atomic a;
+      _signal.get();
+      mail = *_pread++;
+      if(_pread == &_mails[Size])
+        _pread = _mails;
+    }
+    
+    // alternative API for zero-copy operation
+    // however, best used with single producer single consumer!
+
+    bool reserve_isr(Mail_t** mail)
+    {
+      assert(write_dirty == false);
+      bool ret = false;
+      if(_signal.count() != Size)
+      {
+        mail = _pwrite;
+        ret = true;
+        assert(write_dirty = true);
+      }
+      return ret;
+    }
+
+    bool try_reserve(Mail_t** mail)
+    {
+      Atomic a;
+      return reserve_isr(mail);
+    }
+
+    void reserve(Mail_t** mail)
+    {
+      assert(write_dirty == false);
+      Atomic a;
+      while(_signal.count() == Size)
+        Routine::yield();
+      assert(write_dirty = true);
+      mail = _pwrite;
+    }
+
+    void release_isr()
+    {
+      assert(write_dirty == true);
+      _pwrite++;
+      assert((write_dirty = false) == false);
+      _signal.give_isr();
+    }
+
+    void release()
+    {
+      Atomic a;
+      release_isr();
+    }
+
+    bool peek_isr(Mail_t*& mail)
+    {
+      assert(read_dirty == false);
+      bool ret = false;
+      if(_signal.count() != 0)
+      {
+        assert(read_dirty = true);
+        mail = _pread;
+        ret = true;
+      }
+      return ret;
+    }
+
+    bool try_peek(Mail_t*& mail)
+    {
+      Atomic a;
+      return peek_isr(mail);
+    }
+
+    void peek(Mail_t*& mail)
+    {
+      while(!try_peek(mail))
+        Routine::yield();
+    }
+
+    void purge_isr()
+    {
+      assert(read_dirty == true);
+      assert(_signal.get_isr());
+      if(++_pread == &_mails[Size])
+        _pread = _mails;
+      assert((read_dirty = false) == false);
+    }
+
+    void purge()
+    {
+      Atomic a;
+      purge_isr();
+    }
+  private:
+    Mail_t *_pread;
+    Mail_t *_pwrite;
+    Semaphore _signal;
+    Mail_t _mails[Size];
+#ifdef DEBUG
+    bool read_dirty, write_dirty;
+#endif
   };
 
   template<uint8_t Index, typename Functor_t, typename Argument_t>
   inline void Routine::init(Functor_t functor, Argument_t arg, uint8_t* stack, uint16_t stacksize, uint8_t rr_millis)
   {
-    static_assert(Index < SYN_OS_ROUTINE_COUNT, "Routine Index out of bounds!");
+    static_assert(Index < SYN_OS_ROUTINE_COUNT, "Routine Index out of bounds! Increase the ammount specified in the config");
     stack = Kernel::_base_stack_setup(stack, stacksize);
     assert((rr_millis / SYN_SYSTICK_FREQ) > 0);
 #ifndef SYN_OS_ROUTINE_RR_SLICE_MS
@@ -100,9 +308,9 @@ namespace syn
   }
 
   template<uint8_t Index, uint8_t Reload>
-    inline void SoftTimer::init(SoftTimer::timer_functor_t functor)
+  inline void SysTickHook::init(SysTickHook::timer_functor_t functor)
   {
-    static_assert(Index < SYN_OS_TIMER_COUNT, "Timer Index out of bounds!");
+    static_assert(Index < SYN_OS_TICK_HOOK_COUNT, "Timer Index out of bounds!");
     static_assert(Reload > 0, "Timer needs at least Reload of 1");
     _timerlist[Index]._init(functor, Reload);
   }
