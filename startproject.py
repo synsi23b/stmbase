7696679b68c39365e70710207519858be3d2fb32
synsi <mail@himitsu.dev>
2021-01-09 17:49:38 +0900

    startproject script to use absolut path and made heap really optional

--- startproject.py
+++ startproject.py
@@ -11,18 +11,21 @@ def main():
         3: 'stm8s103'
     }
     is_stm8 = [3]
-
-    chiphelp = "for which chip to initialize the project (by number).\n Available choices:\n"
+    chiphelp = "\nAvailable chips: \n"
     for k, v in chips.items():
         chiphelp += f"{k}:\t{v}\n"
 
     parser = argparse.ArgumentParser(
         description='Generate a segger embedded studio or iar workbench project according to target processor')
-    parser.add_argument('chip', type=int, help=chiphelp)
     parser.add_argument(
-        'heap', type=int, help="how much bytes of heap to allocate. Default 8kb, ignored for stm8", default=8192)
-    args = parser.parse_args()
+
+        'chip', type=int, help="for which chip to initialize the project (by number)")
+    parser.add_argument(
+        '--heap', type=int, default=8192, help="how much bytes of heap to allocate. Default 8kb, ignored for stm8")
+
     parser.print_help()
+    print(chiphelp)
+    args = parser.parse_args()
 
     if args.chip not in chips:
         print("Invalid chip chosen, aborting")
@@ -30,6 +33,7 @@ def main():
 
     chipname = chips[args.chip]
     submodule_folder = Path(__file__).parent
+    submodule_folder = submodule_folder.absolute()
     project_folder = submodule_folder.parent
     project_name = project_folder.name
 
