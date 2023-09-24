a26316574a5c34c4374485a1f0fd3dcb99114d3f
Johannes Voelker <mail@himitsu.dev>
2023-09-24 10:42:29 +0900

    Add exponent filtered speed calculation and configuration to AS5600 sens

--- drivers/as5600/as5600.h
+++ drivers/as5600/as5600.h
@@ -20,16 +20,18 @@ public:
   {
     _angle = 0;
     _status_interval = status_update_interval;
-    _current_interval = 0;
+    _current_interval = 1;
     _status = 0;
     _i2c.init(i2c_num, 0x36 << 1, i2c_remap);
     if(_update_status())
     {
-      _current_interval = status_update_interval;
-    }
-    else
-    {
-      _current_interval = 1;
+      _configure();
+      // reset the read address to expected raw angle after configure call
+      uint8_t data = 0x0C;
+      if(_i2c.write(&data, 1) == true)
+      {
+        _current_interval = status_update_interval;
+      }
     }
   }
 
@@ -53,6 +55,11 @@ public:
   int16_t angle() const
   {
     return _angle;
+  }
+
+  uint32_t timestamp() const
+  {
+    return _timestamp;
   }
 
   // returns the raw bitwise status word of the sensor
@@ -89,6 +96,16 @@ public:
   {
     return _status & 0x80;
   }
+
+  uint16_t read_conf()
+  {
+    uint8_t data[2] = {0x07, 0};
+    if(_i2c.write(data, 1) && _i2c.read(data, 2))
+    {
+      return uint16_t(data[0]) << 8 | uint16_t(data[1]);
+    }
+    return 0xFFFF;
+  }
 private:
   uint8_t _update_status()
   {
@@ -97,10 +114,12 @@ private:
     uint8_t data[3] = {0x0B, 0, 0};
     if(_i2c.write(data, 1) && _i2c.read(data, 3))
     {
+      OS_TIME t_measure = OS_TIME_Get_us();
       // Anding really neccessairy? Yes it is.
       _status = data[0] & 0x38;
       if(magnet_present())
       {
+        _timestamp = t_measure;
         _angle = uint16_t(data[1]) << 8 | uint16_t(data[2]);
         // set read address to raw angle for next angle update to skip address transmission
         // only if status is not to be read out everytime
@@ -126,12 +145,29 @@ private:
     return next_interval;
   }
 
+  void _configure()
+  {
+    // reg 0x07
+    // FTH 010b -> 7 / 1 threshold
+    // SF 01b -> 1.1ms settling time
+    // reg 0x08
+    // HYST 10b -> 2 LSB
+    uint8_t data[3] = {0x07, 0x05, 0x04};
+    if(!_i2c.write(data, 3))
+    {
+      // failed to configure, try again in 10ms
+      syn::Thread::sleep(10);
+      _i2c.write(data, 3);
+    }
+  }
+
   void _update_angle()
   {
     uint8_t data[2];
     // address should be set to raw angle already by update method
     if(_i2c.read(data, 2))
     {
+      _timestamp = OS_TIME_Get_us();
       _angle = uint16_t(data[0]) << 8 | uint16_t(data[1]);
     }
     else
@@ -141,12 +177,11 @@ private:
   }
 
   syn::I2cMaster _i2c;
+  uint32_t _timestamp; // the actual stamp is u64, but u32 is more than enough
   uint16_t _angle;
   uint8_t _status_interval;
   uint8_t _current_interval;
   uint8_t _status;
-  //uint8_t _gain;
-  //uint16_t _magnitude;
 };
 
 // run a AS5600 internally, but keep track of the position when its possible to
@@ -162,10 +197,14 @@ public:
   // e.G. set it to 10 and status gets read every 10th update call
   // this is to expedite the angle reading, since we dont have to transmit the
   // angle register address for every read.
+  // the direction parameter controls wether the sensor counts up or down in a specific direction
+  // this can also be selected via pin on the chip directly, might be the better solution in the long run
   void init(uint16_t i2c_num, bool i2c_remap, uint8_t status_update_interval)
   {
     _sensor.init(i2c_num, i2c_remap, status_update_interval);
+    _prev_timestamp = 0;
     _pos = 0;
+    _speed = 0;
     int16_t angle = _sensor.angle();
     // possibly the value is -1 on sensor failure
     // if the sensor failed, set previous to 0
@@ -181,7 +220,7 @@ public:
   // 0x20 == magnet present
   // 0x01 == magnet weak
   // 0x08 == magnet strong
-  uint8_t status()
+  uint8_t status() const
   {
     return _sensor.status();
   }
@@ -196,7 +235,7 @@ public:
   // run regularily to update angle and state
   // state gets updated every status_update_interval ticks
   // if the state update fails for any reason, it will be attempted at the next tick again.
-  void update()
+  bool update()
   {
     _sensor.update();
     if(_sensor.magnet_present())
@@ -205,14 +244,15 @@ public:
       // get the current angle
       int16_t angle = _sensor.angle();
       if(angle == -1)
-        return;
+        return false;
       // calculate the change between the values
       // angle > _prev --> positive value or backwards with 0 point crossing
       // angle < prev --> negative value or forward with 0 point crossing
       // angle == prev --> zero value
       int32_t change = angle - _prev_angle;
+      _prev_angle = angle;
       // if the rate of change per update is bigger than half of the sensor resolution, we cant tell what happened
-      // reading one sensor takes about 0.2ms when reading the status, too
+      // reading one sensor takes about 0.3ms when reading the status, too
       // but limiting to an update rate of 100Hz limits the maximum RPS to 50, or 3000 RPM, which is okay for steppers
       // than again, running a PID loop that mainly reads just the raw angle, could go over 1000Hz easily
       if(change > 2047)
@@ -223,18 +263,42 @@ public:
       {
         change = 4095 + change;
       }
-
       _pos += change;
-      _prev_angle = angle;
+
+      uint32_t elapsed_us = _sensor.timestamp() - _prev_timestamp;
+      _prev_timestamp = _sensor.timestamp();
+      int32_t sptmp = _speed * 15;
+      sptmp += (change * 1000000) / int32_t(elapsed_us);
+      _speed = sptmp / 16;
+
+      return true;
     }
+    return false;
   }
 
-  int32_t position()
+  int32_t position() const
   {
     return _pos;
+  }
+
+  int32_t speed() const
+  {
+    return _speed;
+  }
+
+  uint32_t timestamp() const
+  {
+    return _sensor.timestamp();
+  }
+
+  uint16_t read_conf()
+  {
+    return _sensor.read_conf();
   }
 private:
   AS5600 _sensor;
   int32_t _pos;
+  int32_t _speed;
+  uint32_t _prev_timestamp;
   int16_t _prev_angle;
 };
